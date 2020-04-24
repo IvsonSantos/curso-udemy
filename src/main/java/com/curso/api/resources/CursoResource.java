661300e3cc5234c49f5e9bbc35cdf1c090e6185c
Ivson Santos <ivson.java@gmail.com>
2020-04-24 15:41:17 +0100

    Mapper e salvando curso

--- src/main/java/com/curso/api/resources/CursoResource.java
+++ src/main/java/com/curso/api/resources/CursoResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.curso.api.entities.Curso;
 import com.curso.api.entities.dto.CursoDTO;
+import com.curso.api.entities.mapper.CursoMapper;
 import com.curso.api.services.CursoService;
 
 @RestController
@@ -24,6 +25,9 @@ public class CursoResource {
 	
 	@Autowired
 	private CursoService cursoService;
+	
+	@Autowired
+	private CursoMapper mapper;
 
 	@GetMapping
 	public ResponseEntity<List<Curso>> getCursos() {
@@ -33,14 +37,12 @@ public class CursoResource {
 	
 	@PostMapping("/salva")
 	public ResponseEntity<Curso> saveCurso(
-			@Valid @RequestBody CursoDTO curso) throws URISyntaxException {
+			@Valid @RequestBody CursoDTO dto) throws URISyntaxException {
 		
-//		Curso novoCurso = cursoService.save(curso);
-//		
-//		return ResponseEntity.created(new URI("/cursos/salva/" + novoCurso.getId()))
-//							 .body(novoCurso);
+		Curso novoCurso = cursoService.save(mapper.mapCursoDTOToCurso(dto));
 		
-		return null;
+		return ResponseEntity.created(new URI("/cursos/salva/" + novoCurso.getId()))
+							 .body(novoCurso);
 	}
 	
 }
