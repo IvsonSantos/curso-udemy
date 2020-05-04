520aa7002584d8641ee892ec7afe0f5e5f188958
Ivson Santos <ivson.java@gmail.com>
2020-05-04 19:37:39 +0100

    Assert

---
+++ src/test/java/com/curso/api/curso/CursoTeste.java
@@ -0,0 +1,44 @@
+package com.curso.api.curso;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.math.BigDecimal;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.util.Assert;
+
+import com.curso.api.entities.Curso;
+
+class CursoTeste {
+	
+	@Test
+	void test() {
+//		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testaCursosNaoVazio() {
+		Curso c = new Curso();
+		assertNotNull(c);
+	}
+	
+	@Test
+	void valorDoCursoNaoNulo() {
+		Curso c = new Curso();
+		assertNotNull(c.getValorDoCurso());
+	}
+	
+	@Test
+	void testaNovoCursoValorZero() {
+		Curso curso = new Curso();
+		Assertions.assertEquals(new BigDecimal(0), curso.getValorDoCurso());
+	}
+	
+	@Test
+	void testaNovoCursoSemAlunos() {
+		Curso curso = new Curso();
+		Assertions.assertTrue(curso.getAlunos().isEmpty());
+	}
+
+}
