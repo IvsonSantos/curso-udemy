a089905dffdca379f5fa4dd6a28d33bddf2b8c57
Ivson Santos <ivson.java@gmail.com>
2020-05-06 21:13:41 +0100

    Find by Id

--- src/main/java/com/curso/api/resources/CursoResource.java
+++ src/main/java/com/curso/api/resources/CursoResource.java
@@ -9,6 +9,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -43,6 +44,14 @@ public class CursoResource {
 		
 		return ResponseEntity.created(new URI("/cursos/salva/" + novoCurso.getId()))
 							 .body(novoCurso);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Curso> findCurso(@PathVariable Integer id) {
+		
+		Curso curso = cursoService.findById(id);
+		
+		return ResponseEntity.ok().body(curso);
 	}
 	
 }
